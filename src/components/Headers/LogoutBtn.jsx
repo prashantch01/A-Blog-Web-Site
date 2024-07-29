f1b9cc4fdb2e4e5272daa8c1ecf923eda5aa1bed
Prashant Chakrawarti <prashant00944@gmail.com>
2024-07-29 16:34:34 +0530

    first commit

---
+++ src/components/Headers/LogoutBtn.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import {useDispatch} from 'react-redux'
+// import {authService} from '../../appwrite/auth'
+import authService   from '../../appwrite/config'
+import {logout} from '../../store/authSlice'
+// import authService from '/src/appwrite/auth.js';
+
+
+
+
+
+
+function LogoutBtn() {
+    const dispatch = useDispatch()
+
+    const logoutHandler = () =>{
+        authService.logout().then(()=>{
+            dispatch(logout())
+        })
+    }
+  return (
+    <button className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full' onClick={logoutHandler}>
+        Logout
+    </button>
+  )
+}
+
+export default LogoutBtn
---
+++ src/components/index.js
@@ -0,0 +1,34 @@
+import Header from "./Headers/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Headers/LogoutBtn";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login"
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Input from './Input'
+import Select from './Select'
+import Button from './Button'
+
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    PostForm,
+    PostCard ,
+    AuthLayout,
+    Input,
+    Select,
+    Button,
+    
+ 
+}
